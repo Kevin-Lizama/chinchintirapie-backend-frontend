898b86741a8ac8007865a0db4a129b1fa4219a59
Kevin Lizama <kevinp.lizama@gmail.com>
2026-05-23 20:15:07 -0400

    fix: AutoConfigureMockMvc web to test

--- backend/src/test/java/com/bootcamp/chinchintirapie/BackendTests.java
+++ backend/src/test/java/com/bootcamp/chinchintirapie/BackendTests.java
@@ -8,7 +8,7 @@ import com.bootcamp.chinchintirapie.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
